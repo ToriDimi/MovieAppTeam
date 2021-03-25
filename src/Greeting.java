866c6ff0c2ee4f4ba3703a0beae31f1845125246
radokolev99 <radoslav.kolev@abv.bg>
2021-03-25 09:58:20 +0200

    Changed greetGerman to greetItalian

--- src/Greeting.java
+++ src/Greeting.java
@@ -11,7 +11,7 @@ public class Greeting {
 		System.out.println("Hello World!");
 	}
 
-	public void greetGerman() {
-		System.out.println("Hallo Welt!");
+	public void greetItalian() {
+		System.out.println("Bella Ciao!");
 	}
 }

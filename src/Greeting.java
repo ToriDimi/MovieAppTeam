f280b96a5043fbc3585ab50efde924f4034477ac
radokolev99 <radoslav.kolev@abv.bg>
2021-03-25 09:55:00 +0200

    Added Greeting.java

---
+++ src/Greeting.java
@@ -0,0 +1,17 @@
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting gr = new Greeting();
+		gr.greetEnglish();
+	}
+	
+	public void greetEnglish() {
+		System.out.println("Hello World!");
+	}
+
+	public void greetGerman() {
+		System.out.println("Hallo Welt!");
+	}
+}
